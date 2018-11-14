cd194e8d39db0dbd1a78c584f3c4b1609299ef8d
Elliott Grieco <elliott.grieco@nyu.edu>
2018-11-14 15:05:28 -0500

    Adds simple authentication through token query string

--- handler.js
+++ handler.js
@@ -3,14 +3,18 @@
 const axios = require('axios');
 
 module.exports.campusmedia = async (event, context) => {
-  const { JENKINS_API_KEY, JENKINS_USERNAME } = process.env;
-
+  const { JENKINS_API_KEY, JENKINS_USERNAME, AUTH_TOKEN } = process.env;
   const { token } = event.queryStringParameters;
+
+  if (token !== AUTH_TOKEN) {
+    return handleError({ event, error: "Invalid token.", statusCode: 403 });
+  }
 
   let response;
   try {
-    response = await axios.get(
-      `http://jenkins.library.nyu.edu:8080/view/Campus%20Media/job/Campus%20Media%20Development%20Cron/build/api?token=${token}`,
+    response = await axios.post(
+      `http://jenkins.library.nyu.edu/view/Campus%20Media/job/Campus%20Media%20Development%20Cron%Deploy/build/api`,
+      {}, // empty data
       {
         auth: {
           username: JENKINS_USERNAME,
@@ -31,11 +35,11 @@ function handleSuccess(successObj) {
   };
 }
 
-function handleError({ event, error }) {
+function handleError({ event, error, statusCode = 422, }) {
   console.error(error);
 
   return {
-    statusCode: 422,
+    statusCode,
     body: JSON.stringify({
       message: "We were unable to process this request. Please revise your request.",
       input: event,
